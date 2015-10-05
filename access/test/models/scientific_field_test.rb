f3783d9a42995aa1060c9269cb41bc5938e5ff62
Steve Laskaridis <stevelaskaridis@gmail.com>
2015-10-05 17:11:54 +0300

    ScientificField tests.

--- access/test/models/scientific_field_test.rb
+++ access/test/models/scientific_field_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ScientificFieldTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_params
+    {
+        description: 'Scientific field description',
+    }
+  end
+
+  def test_valid_scientific_field_record
+    valid_rec = ScientificField.new(valid_params)
+    assert valid_rec.valid? "Can't create with valid params: #{valid_rec.errors.messages}"
+  end
+
+  def test_without_compulsory_field
+    compulsory_fields = [:description]
+    compulsory_fields.each do |cf|
+      invalid_params = valid_params.clone
+      invalid_params.delete cf
+      invalid_rec = ScientificField.new(invalid_params)
+      refute invalid_rec.valid?, "Can't be valid without #{cf}"
+      assert invalid_rec.errors[cf], "#{cf} can't be blank."
+    end
+  end
+
 end
