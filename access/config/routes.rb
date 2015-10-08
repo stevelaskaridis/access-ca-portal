6da54fa1b4b7408b82d723a6991f929dd0d45fee
Steve Laskaridis <stevelaskaridis@gmail.com>
2015-10-08 19:10:18 +0300

    Mailer support for new mails of the user.

--- access/config/routes.rb
+++ access/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'people#index', locale: :en
-  get '/:locale' => 'people#index'
+  get '/:locale' => 'people#index', as: 'verify_email'
   scope ":locale", locale: /#{APP_CONFIG['available_locales'].join('|')}/ do
       resources :people
       get '/people/confirm_email/:token' => 'people#verify_email'
