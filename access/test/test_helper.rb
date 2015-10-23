68af72c668b579099ea16f54a469ca0928bfdd91
Nicolas Liampotis <nliam@grnet.gr>
2015-10-23 12:43:29 +0300

    Load SimpleCov Rcov formatter from test/test_helper.rb (refs #21)

--- access/test/test_helper.rb
+++ access/test/test_helper.rb
@@ -1,5 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
