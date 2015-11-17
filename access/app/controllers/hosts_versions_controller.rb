336c5d931d356d2da42beff84ea1194c602cbf84
Steve Laskaridis <stevelaskaridis@gmail.com>
2015-11-18 00:51:43 +0100

    Hosts(versions) controller authorization.

--- access/app/controllers/hosts_versions_controller.rb
+++ access/app/controllers/hosts_versions_controller.rb
@@ -1,6 +1,12 @@
 class HostsVersionsController < ApplicationController
+  before_filter :authorize!
   def index
     @host = Host.find(params[:host_id])
     @host_versions = @host.versions
   end
+
+  private
+    def authorize!
+      redirect_to hosts_url, alert: "Not authorized!" unless TmpAdmin.is_admin?(current_user)
+    end
 end
