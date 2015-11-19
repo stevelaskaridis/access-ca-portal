e1b2cadc106dbeebd332a41696018e198d3bac06
Nasos Rossikopoulos <nassos@arossiko@lab.grid.auth.gr>
2015-11-19 02:05:26 +0200

    Users can see the status of their csr

--- access/app/controllers/csr_gen_controller.rb
+++ access/app/controllers/csr_gen_controller.rb
@@ -30,6 +30,25 @@ class CsrGenController < ApplicationController
 
   def error_csr_sub
   end
-  
+
+  def csr_value
+    email = params[:emailAddress]
+    @person = Person.find_by(:email => email)
+    if @person
+      id = @person.id
+      @status = CertificateRequest.find_by(:id => id)
+      if @status
+        render :inline => '<pre><%= @status.status %></pre>'
+      else
+        redirect_to :action => "error"
+      end
+    else
+      redirect_to :action => "error"
+    end
+
+  end
+
+  def error
+  end
 
 end
