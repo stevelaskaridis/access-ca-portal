a1efbf8f53f43c1ba1f3002e8a3d55514e65ebfa
Steve Laskaridis <stevelaskaridis@gmail.com>
2016-01-04 02:23:24 +0100

    Access certificates/certificate requests from distinguished_names/person.

--- access/app/models/distinguished_name.rb
+++ access/app/models/distinguished_name.rb
@@ -3,4 +3,6 @@ class DistinguishedName < ActiveRecord::Base
 
   validates :subject_dn, presence: true, dn: true
   validates_inclusion_of :owner_type, in: %w(Person Host)
+  has_many :certificate_requests, foreign_key: :owner_dn_id
+  has_many :certificates
 end
